eb44dbdb271235d9ba3fcd7370302e5b0e8669e4
Enrico Mtt <emottus@tktk.ee>
2023-01-29 12:47:42 +0200

    create new app

---
+++ FirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// This is a comment
+
+//Rakendus küsib kasutajanime
+//Rakendus tervitab 
+
+Console.WriteLine("Enter your name:");
+//String - sõne
+String userName = Console.ReadLine();
+
+Console.WriteLine("Hello, " + userName);
+
+//String interpolation
+Console.WriteLine($"Hello, {userName}!");
